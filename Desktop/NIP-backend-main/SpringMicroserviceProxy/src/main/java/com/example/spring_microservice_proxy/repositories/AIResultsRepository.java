b3a6843a4672290b1399c668a9df8158b0f8955c
matprzygocki <118920354+matprzygocki@users.noreply.github.com>
2024-06-13 18:50:27 +0200

    Delete

--- Desktop/NIP-backend-main/SpringMicroserviceProxy/src/main/java/com/example/spring_microservice_proxy/repositories/AIResultsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring_microservice_proxy.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AIResultsRepository extends JpaRepository<AIResultJPAEntity, String> {
-}
